5e81b8dd33fdd2fb870f9446439bfb1566f73292
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-10-10 14:56:57 +0700

    CRUD

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,15 @@
 <?php
 Route::get("/dashboard", [\App\Http\Controllers\AdminController::class, "dashboard"]);
 Route::get("/category", [\App\Http\Controllers\AdminController::class, "dashboard"]);
-Route::get("/product", [\App\Http\Controllers\AdminController::class, "dashboard"]);
+
+Route::prefix("product")->group(function (){
+    Route::get("/", [\App\Http\Controllers\ProductController::class, "index"]);
+    Route::get("/create", [\App\Http\Controllers\ProductController::class, "create"]);
+    Route::post("/create", [\App\Http\Controllers\ProductController::class, "store"]);
+    Route::get("/edit/{product}", [\App\Http\Controllers\ProductController::class, "edit"]);
+    Route::put("/edit/{product}", [\App\Http\Controllers\ProductController::class, "update"]);
+    Route::delete("/delete/{product}", [\App\Http\Controllers\ProductController::class, "delete"]);
+});
+
+
 Route::get("/order", [\App\Http\Controllers\AdminController::class, "orders"]);
